2d475bb8603c899d71a51b186be1492e69252410
Owen <jiawei.me@hotmail.com>
2023-03-28 17:11:32 +0800

    add select chat-model page, is context mode or not

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -11,16 +11,12 @@ type Message = {
     content: string
 }
 export default function ChatBot() {
+    const [isContext, setIsContext] = useState<boolean>(false);
+    const [isSelected, setIsSelected] = useState<boolean>(false);
     const [message, setMessage] = useState<string>("");
-    const [messages, setMessages] = useState<Array<Message>>([{
-        role: 1,
-        time: new Date().toLocaleTimeString(),
-        content: "您好，我是您的私人助理"
-    } as Message]);
+    const [messages, setMessages] = useState<Array<Message>>([]);
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [curRandomId, setCurRandomId] = useState<string>(uuidv4());
-    const [botMessage, setBotMessage] = useState<string>("");
-    const useStore = create((set) => ([]))
 
     const {register, handleSubmit, formState: {errors}} = useForm({
         values: {
@@ -47,7 +43,8 @@ export default function ChatBot() {
         setMessage("");
         get("/api/gpt/chat", {
             message: message,
-            id: curRandomId
+            id: curRandomId,
+            isContext: isContext
         }).then((res: any) => {
             // let msgs = messages;
             let msg: Message = messages[messages.length - 1];
@@ -81,7 +78,7 @@ export default function ChatBot() {
 
     }
 
-    const deleteContext = (id:string) => {
+    const deleteContext = (id: string) => {
         get("/api/gpt/clear", {
             id: curRandomId
         }).then((res: any) => {
@@ -119,56 +116,115 @@ export default function ChatBot() {
     };
 
     return (
-        <div
-            className={"chat-gpt p-0 m-5  lg:m-10 overflow-y-auto flex-grow max-h-full card bg-base-300 relative justify-between"}>
+        isSelected ?
+            (<div className={"chat-gpt p-0 m-5  lg:m-10 overflow-y-auto flex-grow max-h-full card bg-base-300 relative justify-between"}>
 
-            <div className={"mt-0 mb-5"}></div>
-            <div id={"chats"} className={"lg:m-10 m-3 overflow-auto h-full mb-10 chat-area no-scrollbar"}>
-                {
-                    messages.map((msg, index) => {
-                        return (
-                            <div key={index} className={"chat " + (msg.role === 1 ? "chat-start" : "chat-end")}>
-                                <div className="chat-image avatar">
-                                    <div className="w-10 rounded-full">
-                                        <img src={msg.role === 1 ? "/OIP.jpg" : "/avatar.png"} alt={""}/>
+                <div className={"mt-0 mb-5"}>
+
+                </div>
+
+                <div id={"chats"} className={"lg:m-10 m-3 overflow-auto h-full mb-10 chat-area no-scrollbar"}>
+                    {
+                        messages.map((msg, index) => {
+                            return (
+                                <div key={index} className={"chat " + (msg.role === 1 ? "chat-start" : "chat-end")}>
+                                    <div className="chat-image avatar">
+                                        <div className="w-10 rounded-full">
+                                            <img src={msg.role === 1 ? "/OIP.jpg" : "/avatar.png"} alt={""}/>
+                                        </div>
                                     </div>
+                                    <div className="chat-header">
+                                        {/*BOT*/}
+                                        <time className="text-xs opacity-50">{msg.time}</time>
+                                    </div>
+                                    <div className="chat-bubble whitespace-pre-line text-left">{msg.content}</div>
+                                    {/*<div className="chat-footer opacity-50">*/}
+                                    {/*    Delivered*/}
+                                    {/*</div>*/}
                                 </div>
-                                <div className="chat-header">
-                                    {/*BOT*/}
-                                    <time className="text-xs opacity-50">{msg.time}</time>
-                                </div>
-                                <div className="chat-bubble whitespace-pre-line text-left">{msg.content}</div>
-                                {/*<div className="chat-footer opacity-50">*/}
-                                {/*    Delivered*/}
-                                {/*</div>*/}
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            <div
-                className={"absolute m-0 ml-auto mr-auto bottom-10 message-area w-4/6  no-scrollbar relative bg-transparent"}>
-                <form className={"no-scrollbar"} onSubmit={handleSubmit(sendMessage)}>
+                            )
+                        })
+                    }
+                </div>
+                <div
+                    className={"absolute m-0 ml-auto mr-auto bottom-10 message-area w-4/6  no-scrollbar relative bg-transparent"}>
+                    <form className={"no-scrollbar"} onSubmit={handleSubmit(sendMessage)}>
                     <textarea
                         {...register("message", {required: true})}
                         placeholder="在此输入消息，Enter发送"
                         onKeyDown={commentEnterSubmit}
                         className={"bg-base-200 textarea rounded-xl shadow-2xl resize-none w-full  no-scrollbar break-all"}>
                     </textarea>
-                    {errors.message && <p className={"text-red-500 absolute"}>message is required.</p>}
-                    <button
-                        type={"submit"}
-                        disabled={isLoading}
-                        className="border-0 disabled:cursor-not-allowed absolute bg-base-200 p-1 rounded-md text-gray-500 bottom-1.5 md:bottom-2.5 hover:border-0 hover:bg-base-300 focus:border-0 dark:disabled:hover:bg-transparent right-1 md:right-2">
-                        <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round"
-                             strokeLinejoin="round" className="h-4 w-4 mr-1" height="1em" width="1em"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <line x1="22" y1="2" x2="11" y2="13"></line>
-                            <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
-                        </svg>
-                    </button>
-                </form>
-            </div>
-        </div>
+                        {errors.message && <p className={"text-red-500 absolute"}>message is required.</p>}
+                        <button
+                            type={"submit"}
+                            disabled={isLoading}
+                            className="border-0 disabled:cursor-not-allowed absolute bg-base-200 p-1 rounded-md text-gray-500 bottom-1.5 md:bottom-2.5 hover:border-0 hover:bg-base-300 focus:border-0 dark:disabled:hover:bg-transparent right-1 md:right-2">
+                            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24"
+                                 strokeLinecap="round"
+                                 strokeLinejoin="round" className="h-4 w-4 mr-1" height="1em" width="1em"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <line x1="22" y1="2" x2="11" y2="13"></line>
+                                <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
+                            </svg>
+                        </button>
+                    </form>
+                </div>
+            </div>)
+            :
+            (<div className={"flex flex-col justify-center align-middle "}>
+                <div className={"lg:mt-0 lg:mb-5"}>
+
+                </div>
+                <div className={"flex flex-col"}>
+                    <div>
+                        <p className={"text-xl font-bold"}>选择对话模式</p>
+                    </div>
+                    <div className={"divider"}></div>
+                </div>
+                <div className={"flex flex-row flex-wrap justify-center align-middle"}>
+                    <div className="card p-0 w-96 bg-base-300 shadow-xl m-3">
+                        <figure className="hidden lg:px-10 lg:pt-10 lg:flex">
+                            <img src="/OIP.jpg" alt="Shoes" className="hidden lg:rounded-xl lg:block"/>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">普通模式</h2>
+                            <p>普通问答模式，不会记录对话上下文信息，不关联上下文对话</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary" onClick={()=>{
+                                    setIsContext(false);
+                                    setIsSelected(true);
+                                    setMessages([{
+                                        role: 1,
+                                        time: new Date().toLocaleTimeString(),
+                                        content: "您将使用普通模式与我对话。"
+                                    } as Message]);
+                                }}>普通模式</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card p-0 w-96 bg-base-300 shadow-xl m-3">
+                        <figure className="hidden lg:px-10 lg:pt-10 lg:flex">
+                            <img src="/OIP.jpg" alt="Shoes" className="hidden lg:rounded-xl lg:block"/>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">上下文模式</h2>
+                            <p>关联对话上下文，会根据上下文内容做出回答，但是由于gpt的token限制，每次对话次数有限。</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary" onClick={()=>{
+                                    setIsContext(true);
+                                    setIsSelected(true);
+                                    setMessages([{
+                                        role: 1,
+                                        time: new Date().toLocaleTimeString(),
+                                        content: "您将使用上下文模式与我对话。"
+                                    } as Message]);
+                                }}>上下文模式</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>)
     )
 }
