3aec4f4cd04583eaaf529edcf53147ee0489492f
Owen <jiawei.me@hotmail.com>
2023-04-11 11:10:51 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: "/",
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:7788',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
+export default ({mode}) =>
+{
+    return defineConfig({
+
+        plugins: [react()],
+        base: "/",
+        server: {
+            proxy: {
+                '/api': {
+                    target: loadEnv(mode, process.cwd()).VITE_BASE_URL,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                }
             }
         }
-    }
-})
+    })
+}
