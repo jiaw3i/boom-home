9b2f055c328f10e14020c9f352c9fc5734a510d3
Owen <jiawei.me@hotmail.com>
2023-05-11 16:07:07 +0800

    optimized

--- src/components/chatbot/ChatType.tsx
+++ src/components/chatbot/ChatType.tsx
@@ -46,26 +46,24 @@ export default function ChatType() {
                                                                       strokeWidth="2"
                                                                       d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"/>
                                                             </svg>
-
                                                         </button>
                                                     </div>
 
                                                     <div className={bot.enable ? "" : "hover:cursor-not-allowed"}>
-                                                        <button className={"btn btn-primary "} disabled={!bot.enable}>
 
-                                                            <label htmlFor="commonBotConfig"
-                                                                   className={"hover:cursor-pointer w-full h-full flex flex-row justify-center items-center"}
-                                                                   onClick={() => setSettingType(bot.type)}
-                                                                   >
-                                                                <svg className="h-6 w-6 text-white" viewBox="0 0 24 24"
-                                                                     fill="none" stroke="currentColor" strokeWidth="2"
-                                                                     strokeLinecap="round" strokeLinejoin="round">
-                                                                    <circle cx="12" cy="12" r="3"/>
-                                                                    <path
-                                                                        d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"/>
-                                                                </svg>
-                                                            </label>
-                                                        </button>
+                                                        <label htmlFor="commonBotConfig"
+                                                            // className={"hover:cursor-pointer w-full h-full flex flex-row justify-center items-center"}
+                                                               className={"btn btn-primary " + (bot.enable ? " " : "btn-disabled")}
+                                                               onClick={() => setSettingType(bot.type)}
+                                                        >
+                                                            <svg className="h-6 w-6 text-white" viewBox="0 0 24 24"
+                                                                 fill="none" stroke="currentColor" strokeWidth="2"
+                                                                 strokeLinecap="round" strokeLinejoin="round">
+                                                                <circle cx="12" cy="12" r="3"/>
+                                                                <path
+                                                                    d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"/>
+                                                            </svg>
+                                                        </label>
                                                     </div>
 
                                                 </div>
