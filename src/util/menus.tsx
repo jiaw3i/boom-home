2e79a943782f111b012cdd6f56ac2ce1bcfe9207
Owen <jiawei.me@hotmail.com>
2023-07-14 15:13:12 +0800

    修改博客站点链接

--- src/util/menus.tsx
+++ src/util/menus.tsx
@@ -147,10 +147,10 @@ const Links = [
             <line x1="13" y1="12" x2="15" y2="12"/>
         </svg>,
         isShow: true,
-        url: "https://blog.hanjiawei.com"
+        url: "https://hanjiawei.com"
     },
     {
-        title: "网盘💾",
+        title: "网盘",
         icon: <svg className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                   d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"/>
