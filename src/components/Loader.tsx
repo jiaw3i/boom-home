ca5c9bf8eba8128b402db21069880de8eae94859
Owen <jiawei.me@hotmail.com>
2023-07-19 20:12:32 +0800

    优化loader样式

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,13 +1,10 @@
 export default function Loader() {
     return (
-        <div className="flex items-center justify-center min-h-screen p-5 min-w-screen">
-
-            <div className="flex space-x-2 animate-pulse">
-                <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
-                <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
-                <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
-            </div>
-
+        <div className="flex items-center justify-center h-full w-full">
+            {/*<div className="w-3 h-3 bg-gray-500 rounded-full"></div>*/}
+            {/*<div className="w-3 h-3 bg-gray-500 rounded-full"></div>*/}
+            {/*<div className="w-3 h-3 bg-gray-500 rounded-full"></div>*/}
+            <span className="loading loading-spinner loading-lg"></span>
         </div>
     )
 }
