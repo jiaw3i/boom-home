8b363573be955d24a0308635ce02e4caaaeb3d83
Owen <jiawei.me@hotmail.com>
2023-08-02 15:00:33 +0800

    add project nl2type

--- src/util/projects.tsx
+++ src/util/projects.tsx
@@ -2,17 +2,17 @@ const ProjectsData = [
     {
         id: 1,
         name: "boom-home",
-        description: "个人主页前端，React+Ts+TailwindCss+DaisyUI。\n持续开发中",
+        description: "个人主页前端，React+Ts+TailwindCss+DaisyUI。\n",
         gitUrl: "https://github.com/jiaw3i/boom-home",
-        homeUrl: "https://hanjiawei.com",
+        homeUrl: "https://colazy.com",
         cover: "https://github.com/jiaw3i/boom-home",
     },
     {
         id: 2,
         name: "boom-home-api",
-        description: "个人主页后端，SpringBoot+MySQL+Redis+elasticsearch。\n持续开发中",
+        description: "个人主页后端，SpringBoot+MySQL+Redis+elasticsearch。\n",
         gitUrl: "https://github.com/jiaw3i/boom-home-api",
-        homeUrl: "https://hanjiawei.com",
+        homeUrl: "https://colazy.com",
         cover: "https://github.com/jiaw3i/boom-home-api",
     },
     {
@@ -33,6 +33,14 @@ const ProjectsData = [
     },
     {
         id: 5,
+        name: "nl2type",
+        description: "自然语言转为结构化数据，基于microsoft@TypeChat\n",
+        gitUrl: "https://github.com/jiaw3i/nl2type",
+        homeUrl: "https://nl2type.colazy.com",
+        cover: "https://github.com/jiaw3i/nl2type",
+    },
+    {
+        id: 998,
         name: "pure-land",
         description: "净土,烂尾项目react+antd+ts做的，页面太丑了做不下去了；已经被boom-home项目代理，凑个数。",
         gitUrl: "https://github.com/jiaw3i/pureland",
@@ -40,7 +48,7 @@ const ProjectsData = [
         cover: "https://github.com",
     },
     {
-        id: 6,
+        id: 999,
         name: "pure-land后端",
         description: "pure-land后端项目",
         gitUrl: "https://github.com/jiaw3i/always-online",
