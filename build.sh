a61b4f57bf4c863782a505c1833f3ceb63bd94bb
Jackway <jiawei.me@hotmail.com>
2024-03-06 17:05:06 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 接受新版本号作为参数
+new_version=$1
+
+echo "start build image..."
+docker build -t home:$new_version .
+
+echo "build successed.  home:$new_version"
--- start.sh
+++ start.sh
@@ -11,5 +11,5 @@ docker rm home
 # 启动新的容器
 echo "启动新的容器: boom-home:$new_version..."
 # docker run -d --name my_container_$new_version my_image:$new_version
-docker run -itd -p 3080:80 --name=home boom-home:$new_version
+docker run -itd -p 3080:80 --name=home --network=home-bridge boom-home:$new_version
 echo "容器更新完成！"
