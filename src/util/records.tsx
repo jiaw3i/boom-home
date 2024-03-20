037a80065bbb10bee088cb9935977f294502b839
Owen <jiawei.me@hotmail.com>
2024-03-20 14:50:33 +0800

    record集成图片上传

--- src/util/records.tsx
+++ src/util/records.tsx
@@ -1,16 +1,5 @@
-import {RecordInfo} from "@/components/recordwall/RecordWall";
+// import {RecordInfo} from "@/components/recordwall/RecordWall";
 
-const RecordsData:Array<RecordInfo> = [
-    {
-        id: 1,
-        content: "hello #hello  #world。  \n" +
-            "![IMG_20230414_000624.jpg](https://img.hanjiawei.com/owen/2023/06/09/6482ec2f6c260.jpg)" +
-            "<img src='https://img.hanjiawei.com/owen/2023/06/09/6482ec2f6c260.jpg' alt='loading...'/>",
-        permission: 1,
-        tag: "#hello,#world",
-        createTime: "2021-06-09T00:06:24"
-    },
-]
 
 export function fileToBase64(file: File): Promise<string> {
     return new Promise((resolve, reject) => {
@@ -23,4 +12,3 @@ export function fileToBase64(file: File): Promise<string> {
         reader.onerror = (error) => reject(error);
     });
 }
-export default RecordsData;
