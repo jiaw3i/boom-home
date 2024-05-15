78cf06a52744e4cbd03ab82120a7e6830555c1e0
jackway <jiawei.me@hotmail.com>
2024-05-15 14:04:56 +0800

    rss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from "path";
+import {resolve} from "path";
 
 // https://vitejs.dev/config/
-export default ({mode}) =>
-{
+export default ({mode}) => {
     return defineConfig({
 
         plugins: [react()],
@@ -16,6 +15,11 @@ export default ({mode}) =>
                     target: loadEnv(mode, process.cwd()).VITE_BASE_URL,
                     changeOrigin: true,
                     rewrite: (path) => path.replace(/^\/api/, '')
+                },
+                '/blog/atom.xml': {
+                    target: loadEnv(mode, process.cwd()).VITE_BASE_URL,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/blog/, 'post')
                 }
             }
         },
